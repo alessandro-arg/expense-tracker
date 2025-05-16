6edf9aca5e983b8cb1433d6ea6e7fa2b4406b7d6
Alessandro Argenziano <1603alessandro@gmail.com>
2025-05-16 18:06:31 +0200

    imported all the ngZorro modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ZORRO_MODULES } from './zorro-imports';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, ZORRO_MODULES],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'expense-tracker';
