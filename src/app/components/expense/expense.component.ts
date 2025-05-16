23fd11bd354b5931ce57c4110cc5c716c842bc63
Alessandro Argenziano <1603alessandro@gmail.com>
2025-05-16 20:32:51 +0200

    the html of the expense component is now working

--- src/app/components/expense/expense.component.ts
+++ src/app/components/expense/expense.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ZORRO_MODULES } from '../../zorro-imports';
 
 @Component({
   selector: 'app-expense',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ZORRO_MODULES],
   templateUrl: './expense.component.html',
   styleUrl: './expense.component.scss',
 })
 export class ExpenseComponent {
   expenseFrom!: FormGroup;
+  listOfCategory: any[] = [
+    'Education',
+    'Groceries',
+    'Health',
+    'Subscriptions',
+    'Takeaways',
+    'Clothing',
+    'Traveling',
+    'Other',
+  ];
 
   constructor(private fb: FormBuilder) {}
 
